5355e2416040d206c77990526ffc06055e71a88b
nsarumathi <sarumathinallasivam97@gmail.com>
2026-06-28 16:41:11 +0530

    terraform file added

---
+++ terraform-nginx-ubuntu/main.tf
@@ -0,0 +1,108 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get one subnet from the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Ubuntu 20.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "nginx_sg" {
+  name        = "terraform-nginx-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-nginx-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "nginx_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.nginx_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install nginx -y
+              systemctl enable nginx
+              systemctl start nginx
+
+              cat <<HTML > /var/www/html/index.html
+              <html>
+                <head>
+                  <title>Terraform Nginx</title>
+                </head>
+                <body>
+                  <h1>Welcome to the Terraform-managed Nginx Server on Ubuntu</h1>
+                </body>
+              </html>
+              HTML
+              EOF
+
+  tags = {
+    Name = "terraform-nginx-server"
+  }
+}
